505d102a4ad58a02651a2c3ca026129c261419ee
Matt <matt@geeks.ltd.uk>
2021-11-30 10:39:10 +0000

    updates

---
+++ SynkTest/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+
+    public void Register()
+    {
+        string FirstName1 = "hello";
+    }
+
+    public void Register2()
+    {
+        string LastName1 = "student";
+        string FirstName1 = "hello";
+    }
+
+}
